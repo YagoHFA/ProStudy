6b268fadd3013449d11eba81ca58b8a143bb7efe
AnderSecurity <anderson.ander.escola@hotmail.com>
2024-03-23 09:33:34 -0300

    Criado lista de categorias

--- PlanBTec/src/app/components/video-list/video-list.component.ts
+++ PlanBTec/src/app/components/video-list/video-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Category } from '../../class/category';
 import { Video } from '../../class/video';
 
@@ -9,16 +9,6 @@ import { Video } from '../../class/video';
 })
 export class VideoListComponent implements OnInit{
   ngOnInit(): void {
-    this.video.id = "Z6jZUXpsqvc";
-    this.video.titulo = "BigsMacs";
-
-    this.video2.id = "aAgbevbQH-s";
-    this.video2.titulo = "ObservationDiaryes";
-    this.videoList.push(this.video);
-    this.videoList.push(this.video2);
   }
-    category:Category = new Category();
-    video:Video = new Video();
-    video2:Video = new Video();
-    videoList:Video[] = [];
+    @Input() videoList:Video[] = [];
 }
