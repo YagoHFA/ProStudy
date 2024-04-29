521895e262f8bf7d5b77539a437d67ac3333a54b
YagoHFA <yagof2421@gmail.com>
2024-04-29 16:25:05 -0300

    Adicionando controller, repository e service para project

---
+++ prostudy-api/src/main/java/com/planbtech/prostudy/repositories/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.planbtech.prostudy.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository {
+
+}
---
+++ prostudy-api/src/main/java/com/planbtech/prostudy/services/interfaces/IProjectService.java
@@ -0,0 +1,7 @@
+package com.planbtech.prostudy.services.interfaces;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IProjectService {
+}
