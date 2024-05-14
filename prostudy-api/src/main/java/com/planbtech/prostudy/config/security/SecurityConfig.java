5686582dc87bf6933967b20e72302627fd3f6527
YagoHFA <yagof2421@gmail.com>
2024-05-13 22:17:05 -0300

    Adicionando flow para o sprg boot azure

--- prostudy-api/src/main/java/com/planbtech/prostudy/config/security/SecurityConfig.java
+++ prostudy-api/src/main/java/com/planbtech/prostudy/config/security/SecurityConfig.java
@@ -72,7 +72,6 @@ public class SecurityConfig {
 
     private ClientRegistration gmailClientRegistration() {
         return ClientRegistration.withRegistrationId("gmail")
-                .clientId("997799911577-gmm1n3ghikrf5h0e3m97uv5grh32440m.apps.googleusercontent.com")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
