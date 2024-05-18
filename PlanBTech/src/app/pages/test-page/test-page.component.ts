1a5dc8bbf6fa81f377f9d90ad416f201beaf6656
YagoHFA <yagof2421@gmail.com>
2024-05-18 12:28:56 -0300

    Alterando catalogo de testes

--- PlanBTech/src/app/pages/test-page/test-page.component.ts
+++ PlanBTech/src/app/pages/test-page/test-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TestService } from '../../service/controller/test.service';
+import { Test } from '../../class/test';
 
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.css'
 })
-export class TestPageComponent {
-
+export class TestPageComponent implements OnInit{
+constructor(private testService:TestService){}
+  ngOnInit(): void {
+    this.testService.getTesteById().subscribe(
+      (test:Test)=>{
+        this.test = test;
+      }
+    );
+  }
+  @Input() test: Test = new Test();
 }
